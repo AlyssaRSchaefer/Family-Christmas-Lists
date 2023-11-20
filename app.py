e2220233940e4174946c6de36402a1c6ab2d9662
Alyssa <alyssarschaefer@gmail.com>
2023-11-20 00:21:48 -0600

    worked on home page

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, send_file, render_template, request, redirect, session, send_from_directory
+import pymysql
+
+app = Flask(__name__, static_url_path='/static')
+# Database configuration
+db_config = {
+    'host': '127.0.0.1',     
+    'user': 'root',
+    'password': 'password',
+    'database': 'christmas',
+}
+
+# Create a connection to the database
+conn = pymysql.connect(**db_config)
+
+# Function to execute SQL queries
+def execute_query(query, params=None):
+    with conn.cursor() as cursor:
+        cursor.execute(query, params)
+        result = cursor.fetchall()
+    return result
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Get form data
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Example query
+        query = "SELECT * FROM users WHERE name = %s AND password = %s;"
+        params = (username, password)
+        result = execute_query(query, params)
+
+        # Process the result (replace with your logic)
+        if result:
+            print("Login successful!")
+        else:
+            print("Login failed!")
+
+    return render_template('login.html')
+
+if __name__ == '__main__':
+   app.run(debug=True, host='0.0.0.0', port=80)
---
+++ static/javascript/login.js
@@ -0,0 +1,3 @@
+/* IMPLEMENT THE LOGIN MODAL BOX */
+var loginBox = document.getElementById("login-box");
+var loginButton = document.getElementById("login-button");
